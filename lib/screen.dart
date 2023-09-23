ea6aaf8513addcd738a495160951d263be913bbd
mohid hafeez <mohidzohaib544@gmail.com>
2023-09-23 17:49:21 +0500

    finalizing

--- lib/screen.dart
+++ lib/screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:real_estate/utilities/bottom_navigation_bar.dart';
+
 import 'package:real_estate/utilities/row_tile.dart';
 
 class Screen extends StatelessWidget {
@@ -11,140 +12,140 @@ class Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        height: 100,
-        child: CustomNavigtorBar(price: "Rp.1.500.000.000/ Year"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 16.0, right: 16, left: 16),
-        child: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: SizedBox(
-                height: 290,
-                width: 390,
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        bottomNavigationBar: CustomBottomNavigationBar(),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 16.0, right: 16, left: 16),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: SizedBox(
+                  height: 290,
+                  width: 390,
+                  child: Image.asset(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 310.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Desciption",
+              Padding(
+                padding: const EdgeInsets.only(top: 310.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Desciption",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500)),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    const Text(
+                        "the Level 3 house that has the modern design has a large pool and a garage that fit the four cars.",
+                        style: TextStyle(color: Colors.black54)),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundImage: AssetImage(
+                                  'assets/jurica-koletic-7YVZYZeITc8-unsplash_adobespark 1.png'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Garry Allan',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                                Text('Owner',
+                                    style: TextStyle(color: Colors.black54)),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue[300],
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: const Image(
+                                    image: AssetImage('assets/Frame.png'))),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue[300],
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: const Image(
+                                    image: AssetImage('assets/Frame-1.png'))),
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    const Text(
+                      "Gallery",
                       style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Text(
-                      "the Level 3 house that has the modern design has a large pool and a garage that fit the four cars.",
-                      style: TextStyle(color: Colors.black54)),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 25,
-                            backgroundImage: AssetImage(
-                                'assets/jurica-koletic-7YVZYZeITc8-unsplash_adobespark 1.png'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Garry Allan',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 16),
-                              ),
-                              Text('Owner',
-                                  style: TextStyle(color: Colors.black54)),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                              height: 30,
-                              width: 30,
-                              decoration: BoxDecoration(
-                                  color: Colors.blue[300],
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: const Image(
-                                  image: AssetImage('assets/Frame.png'))),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                              height: 30,
-                              width: 30,
-                              decoration: BoxDecoration(
-                                  color: Colors.blue[300],
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: const Image(
-                                  image: AssetImage('assets/Frame-1.png'))),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Text(
-                    "Gallery",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowTile(
-                          image: 'assets/cat-han-aPD1yNUYP0w-unsplash 1.png'),
-                      RowTile(
-                          image:
-                              'assets/jorge-de-jorge-NvqYkDPE0Rw-unsplash 1.png'),
-                      RowTile(
-                          image:
-                              'assets/sidekix-media-EoTUCbv9Jrs-unsplash 1.png'),
-                      RowTile(
-                          image: 'assets/cat-han-aPD1yNUYP0w-unsplash 1.png'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  SizedBox(
-                    height: 80,
-                    width: 500,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Image.asset(
-                        'assets/map.jpeg',
-                        fit: BoxFit.cover,
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RowTile(
+                            image: 'assets/cat-han-aPD1yNUYP0w-unsplash 1.png'),
+                        RowTile(
+                            image:
+                                'assets/jorge-de-jorge-NvqYkDPE0Rw-unsplash 1.png'),
+                        RowTile(
+                            image:
+                                'assets/sidekix-media-EoTUCbv9Jrs-unsplash 1.png'),
+                        RowTile(
+                            image: 'assets/cat-han-aPD1yNUYP0w-unsplash 1.png'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: 500,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.asset(
+                          'assets/map.jpeg',
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/utilities/bottom_navigation_bar.dart
+++ lib/utilities/bottom_navigation_bar.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate/utilities/main_button.dart';
 
-class CustomNavigtorBar extends StatelessWidget {
-  final String price;
-  const CustomNavigtorBar({super.key, required this.price});
-
+class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: Colors.white,
-      elevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.only(right: 8, left: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "price",
-                  style: TextStyle(color: Colors.black54),
-                ),
-                Text(price, style: TextStyle(color: Colors.black, fontSize: 17))
-              ],
-            ),
-            MainButton(text: "Rent Now")
-          ],
-        ),
+    return const BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Price',
+                style: TextStyle(color: Colors.black54),
+              ),
+              Text(
+                'Rp.1.500.000.000 /Years',
+                style: TextStyle(fontSize: 18),
+              )
+            ],
+          ),
+          MainButton(text: 'Rent Now')
+        ],
       ),
     );
   }
